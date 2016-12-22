dcc3704a22339f3837dc6f93bbbfb974ff98db33
jiashiran <jiashiran@jd.com>
2016-12-22 18:37:23 +0800

    init

--- src/gorpc/main.go
+++ src/gorpc/main.go
@@ -1,30 +1,9 @@
 package main
 
-import (
-	"gorpc/api"
-	"gorpc/utils"
-	"time"
-	"log"
-)
 
 func main() {
 
-	rpc := api.NewGoRpc("http://192.168.146.147:2379")
-	ticker := time.NewTicker(3 * time.Second)
-	for range ticker.C{
-		f := api.Facade{
-			Service:"api.Test",
-			Method:"Tostring",
-			Args:api.Request{"ttt protocol rpc"},
-			Response:&api.Response{},
-			Protocol:utils.PROTOCOL_RPC,
-			//Protocol:utils.PROTOCOL_JSON,
-			//Protocol:utils.PROTOCOL_JSON2RPC,
-			//Protocol:utils.PROTOCOL_JSON2RPCHTTP,
-		}
-		rpc.Call(f)
-		log.Println(f.Response)
-	}
+
 }
 
 
